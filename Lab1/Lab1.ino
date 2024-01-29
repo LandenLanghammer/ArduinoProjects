4af54c6935b3ad80dc7acae48867b451191ed7ad
LandenLanghammer <157726249+LandenLanghammer@users.noreply.github.com>
2024-01-29 10:15:39 -0500

    Update Lab1.ino
    Commit 2

--- Lab1/Lab1.ino
+++ Lab1/Lab1.ino
@@ -1,28 +1,27 @@
 //LANDEN
 
+// creates variable strings for 13, 12, and 11
 int red = 13;
 int yellow = 12;
 int green = 11;
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // sets up pins 13, 12, and 11 as outputs
   pinMode(13, OUTPUT);
   pinMode(12,OUTPUT);
   pinMode(11, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(red, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(5000);                      // wait for a second
-  digitalWrite(red, LOW);   // turn the LED off by making the voltage LOW
-  delay(20);  
-  digitalWrite(green, HIGH);   // turn the LED off by making the voltage LOW
-  delay(3000);  
-  digitalWrite(green, LOW);   // turn the LED off by making the voltage LOW
-  delay(20);
-  digitalWrite(yellow, HIGH);   // turn the LED off by making the voltage LOW
-  delay(2000);   
-  digitalWrite(yellow, LOW);  // turn the LED on (HIGH is the voltage level)
-  delay(20);    
+  // LEDs emulating a traffic light
+  digitalWrite(red, HIGH);      // turn the red LED on (HIGH is the voltage level)
+  delay(5000);                  // wati for 5 seconds
+  digitalWrite(red, LOW);       // turn the red LED off by making the voltage LOW
+  digitalWrite(green, HIGH);    // turn the green LED on by making the voltage HIGH
+  delay(3000);                  // wait for 3 seconds
+  digitalWrite(green, LOW);     // turn the green LED off by making the voltage LOW
+  digitalWrite(yellow, HIGH);   // turn the yellow LED off by making the voltage HIGH
+  delay(2000);                  // wait for 2 seconds
+  digitalWrite(yellow, LOW);    // turn the yellow LED off by making the voltage LOW
 
 }
